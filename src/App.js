8f32709300ca08bd04d2064a8e441f4e1fecc59c
Abe <abhi.rawat456@gmail.com>
2021-02-01 17:45:16 +0530

    Added assets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { ChakraProvider, theme } from '@chakra-ui/react';
 
+import Header from './components/Header';
+
 const App = () => {
-  return <ChakraProvider theme={theme}></ChakraProvider>;
+  return (
+    <ChakraProvider theme={theme}>
+      <Header />
+    </ChakraProvider>
+  );
 };
 
 export default App;
