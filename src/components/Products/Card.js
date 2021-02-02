23d11bed1b3a72886b8e393628f1b54fe2c6a704
Abe <abhi.rawat456@gmail.com>
2021-02-02 18:16:47 +0530

    Added hover effects to card

--- src/components/Products/Card.js
+++ src/components/Products/Card.js
@@ -1,16 +1,46 @@
 import { Box, Image, Text } from '@chakra-ui/react';
+import { useState } from 'react';
 
 const Card = ({ image, text }) => {
+  const [overlayColor, setOverlayColor] = useState('black');
+  const [opacity, setOpacity] = useState(0.4);
+
+  const onMouseEnterHandler = () => {
+    setOverlayColor('white');
+    setOpacity(0.7);
+  };
+
+  const onMouseLeaveHandler = () => {
+    setOverlayColor('black');
+    setOpacity(0.4);
+  };
+
   return (
-    <Box position="relative">
+    <Box
+      position="relative"
+      onMouseEnter={onMouseEnterHandler}
+      onMouseLeave={onMouseLeaveHandler}
+    >
       <Image src={image} />
+      <Box
+        bg={overlayColor}
+        opacity={opacity}
+        position="absolute"
+        cursor="pointer"
+        transition="background-color 300ms ease-in-out"
+        top="0"
+        left="0"
+        h="full"
+        w="full"
+      ></Box>
       <Text
         w="32"
         lineHeight="6"
         position="absolute"
         bottom="4"
         left="4"
-        color="white"
+        color={overlayColor === 'white' ? 'black' : 'white'}
+        transition="color 300ms ease-in-out"
         textTransform="uppercase"
         fontSize="xl"
         letterSpacing="widest"
