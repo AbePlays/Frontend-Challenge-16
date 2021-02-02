52c7ce6cda2482066ce81a832b0d7848145d68fe
Abe <abhi.rawat456@gmail.com>
2021-02-02 12:52:08 +0530

    Fixing Header Menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,9 @@ import {
   Link,
   Text,
   useMediaQuery,
+  VStack,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 
 import DesktopBackground from '../../assets/desktop/image-hero.jpg';
 import MobileBackground from '../../assets/mobile/image-hero.jpg';
@@ -16,8 +18,14 @@ const Header = () => {
   const [isLargerThanMobile] = useMediaQuery('(min-width: 30em)');
   const [isLargerThanTablet] = useMediaQuery('(min-width: 48em)');
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const onMenuButtonPressedHandler = () => {
+    setIsMenuOpen(prev => !prev);
+  };
+
   return (
-    <Box position="relative" w="full" h="50vh">
+    <Box position="relative" w="full" h={['100vh', '50vh']}>
       <Image
         src={isLargerThanMobile ? DesktopBackground : MobileBackground}
         objectFit="cover"
@@ -25,6 +33,7 @@ const Header = () => {
         h="full"
       />
       <Box
+        bg={isLargerThanTablet ? 'none' : isMenuOpen ? 'black' : 'none'}
         position="absolute"
         top="0"
         left="0"
@@ -32,21 +41,41 @@ const Header = () => {
         px="4"
         color="white"
         h="full"
-        maxW={['container.sm', 'container.sm', 'container.lg', 'container.lg']}
+        maxW={['container.sm', 'container.md', 'container.lg', 'container.lg']}
         mx="auto"
       >
         <Flex alignItems="center" justifyContent="space-between" h="10vh">
           <Heading>loopstudios</Heading>
           {isLargerThanTablet ? (
             <HStack spacing="6">
-              <Link>About</Link>
-              <Link>Careers</Link>
-              <Link>Events</Link>
-              <Link>Products</Link>
-              <Link>Support</Link>
+              <Link href="#">About</Link>
+              <Link href="#">Careers</Link>
+              <Link href="#">Events</Link>
+              <Link href="#">Products</Link>
+              <Link href="#">Support</Link>
             </HStack>
+          ) : isMenuOpen ? (
+            <svg
+              width="20"
+              height="20"
+              xmlns="http://www.w3.org/2000/svg"
+              cursor="pointer"
+              onClick={onMenuButtonPressedHandler}
+            >
+              <path
+                d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z"
+                fill="#FFF"
+                fill-rule="evenodd"
+              />
+            </svg>
           ) : (
-            <svg width="24" height="16" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              width="24"
+              height="16"
+              xmlns="http://www.w3.org/2000/svg"
+              cursor="pointer"
+              onClick={onMenuButtonPressedHandler}
+            >
               <g fill="#FFF" fill-rule="evenodd">
                 <path d="M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z" />
               </g>
@@ -54,22 +83,39 @@ const Header = () => {
           )}
         </Flex>
         <Flex
-          h="40vh"
+          h={['90vh', '40vh']}
           color="white"
           justifyContent={['center', 'center', 'flex-start']}
           alignItems="center"
         >
-          <Text
-            border="1px"
-            p="4"
-            maxW="80"
-            fontWeight="light"
-            textTransform="uppercase"
-            fontSize="4xl"
-            lineHeight="10"
-          >
-            Immersive Experiences That Deliver
-          </Text>
+          {!isLargerThanTablet && isMenuOpen ? (
+            <VStack
+              spacing="4"
+              textTransform="uppercase"
+              letterSpacing="widest"
+              fontSize="xl"
+              fontWeight="thin"
+            >
+              <Link href="#">About</Link>
+              <Link href="#">Careers</Link>
+              <Link href="#">Events</Link>
+              <Link href="#">Products</Link>
+              <Link href="#">Support</Link>
+            </VStack>
+          ) : (
+            <Text
+              border="1px"
+              px="4"
+              py={['8', '4']}
+              maxW="80"
+              fontWeight="light"
+              textTransform="uppercase"
+              fontSize="4xl"
+              lineHeight="10"
+            >
+              Immersive Experiences That Deliver
+            </Text>
+          )}
         </Flex>
       </Box>
     </Box>
